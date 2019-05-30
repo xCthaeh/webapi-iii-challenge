19a70052fb2e43d2e7187ffc56d65caede156aed
Alyssa Jane <xcthaeh@gmail.com>
2019-05-30 16:20:24 -0400

    env

--- server.js
+++ server.js
@@ -10,8 +10,26 @@ server.use(mws.logger);
 server.use("/api/users", UserRouter);
 server.use("/api/posts", PostRouter);
 
-server.get("/", (req, res) => {
-  res.send(`<h2>Api is working :P </h2>`);
+server.get("/", async (req, res) => {
+  try {
+    const shoutouts = await db("shoutouts");
+    res.status(200).json({ messageOfTheDay: process.env.MOTD, shoutouts });
+  } catch (error) {
+    console.error("\nERROR", error);
+    res.status(500).json({ error: "Cannot retrieve the shoutouts" });
+  }
+});
+
+server.post("/", async (req, res) => {
+  try {
+    const [id] = await db("shoutouts").insert(req.body);
+    const shoutouts = await db("shoutouts");
+
+    res.status(201).json(shoutouts);
+  } catch (error) {
+    console.error("\nERROR", error);
+    res.status(500).json({ error: "Cannot add the shoutout" });
+  }
 });
 
 module.exports = server;
