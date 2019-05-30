dd0f038c46791fabf09889afac1bb625ebca42a5
Alyssa Jane <xcthaeh@gmail.com>
2019-05-30 16:25:07 -0400

    attempt to fix bug

--- server.js
+++ server.js
@@ -10,25 +10,14 @@ server.use(mws.logger);
 server.use("/api/users", UserRouter);
 server.use("/api/posts", PostRouter);
 
-server.get("/", async (req, res) => {
+server.get('/', async (req, res) => {
   try {
-    const shoutouts = await db("shoutouts");
-    res.status(200).json({ messageOfTheDay: process.env.MOTD, shoutouts });
+    const shoutouts = await db('shoutouts');
+    const messageOfTheDay = process.env.MOTD || 'API is working woo :P'; // add this line
+    res.status(200).json({ motd: messageOfTheDay, shoutouts }); // change this line
   } catch (error) {
-    console.error("\nERROR", error);
-    res.status(500).json({ error: "Cannot retrieve the shoutouts" });
-  }
-});
-
-server.post("/", async (req, res) => {
-  try {
-    const [id] = await db("shoutouts").insert(req.body);
-    const shoutouts = await db("shoutouts");
-
-    res.status(201).json(shoutouts);
-  } catch (error) {
-    console.error("\nERROR", error);
-    res.status(500).json({ error: "Cannot add the shoutout" });
+    console.error('\nERROR', error);
+    res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
   }
 });
 
