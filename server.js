d8fa8e284e569a31d61dd444b516f713591da0ee
Alyssa Jane <xcthaeh@gmail.com>
2019-05-30 16:28:16 -0400

    code not working, reverted

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const express = require("express");
 const server = express();
 const UserRouter = require("./users/userRouter");
@@ -11,15 +10,8 @@ server.use(mws.logger);
 server.use("/api/users", UserRouter);
 server.use("/api/posts", PostRouter);
 
-server.get('/', async (req, res) => {
-  try {
-    const shoutouts = await db('shoutouts');
-    const messageOfTheDay = process.env.MOTD || 'API is working woo :P'; // add this line
-    res.status(200).json({ motd: messageOfTheDay, shoutouts }); // change this line
-  } catch (error) {
-    console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
-  }
+server.get("/", (req, res) => {
+  res.send(`<h2>Api is working :P </h2>`);
 });
 
 module.exports = server;
